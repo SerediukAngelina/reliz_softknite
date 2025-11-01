ad104e06f84a26e54dab600405d5191a5eadfb98
SerediukAngelina <SerediukAnhelina@vinnvk29.ukr.education>
2025-11-01 13:26:18 +0200

    day3

--- main.js
+++ main.js
@@ -8,17 +8,54 @@ async function getProducts() {
     return products
 };
 
-function getCardHTML(product){
+function getCardHTML(product) {
     return `<div class="card" style="width: 18rem;">
-  <img src="img/${product.img}" class="card-img-top" alt="...">
+  <img src="img/${product.image}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${product.title}</h5>
     <p class="card-text">${product.price}</p>
-    <a href="#" class="btn btn-primary">Дотати в кошик</a>
+    <a href="#" class="btn btn-primary" data-product='${JSON.stringify(product)}'>Дотати в кошик</a>
   </div>
 </div>`
 };
 
+class ShoppingCart {
+    constructor() {
+        this.items = {} // об’єкт з товарами у кошику
+        this.total = 0  // загальна вартість замовлення
+    }
+
+    addItem(item) { 
+    // Додавання товару до кошика 
+     if(this.items[item.title]){
+        this.items[item.title].quantity +=1;
+     }
+     else{
+        this.items[item.title]=item;
+        this.items[item.title].quantity +=1;
+     }   
+     this.saveCartToCookies()
+    }  
+    	
+    saveCartToCookies() { let cartJSON = JSON.stringify(this.items);
+document.cookie = `cart=${cartJSON}; max-age=${60 * 60 * 24 * 7}; path=/`;
+    }
+
+    loadCartFromCookies() {let cartCookie = getCookieValue('cart');
+if (cartCookie && cartCookie !== '') {
+this.items = JSON.parse(cartCookie);
+    }
+}
+let cart = new ShoppingCart() // Створення об'єкта кошика
+
+
+function addToCart(event) {
+    // Отримуємо дані про товар з data-атрибута кнопки 
+    const productData = event.target.getAttribute('data-product'); 
+    const product = JSON.parse(productData); // перетворюємо JSON у об’єкт 
+    // Тут будемо додавати товар до кошика
+    cart.addItem(product);
+}
 
 // Викликаємо асинхронну функцію та очікуємо на отримання продуктів
 getProducts().then(function (products) {
